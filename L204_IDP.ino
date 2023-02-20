1546c87bce2ab7c7e482f477df2fe884fcf6c8e8
naseweisssss <rxwl2@cam.ac.uk>
2023-02-20 11:28:28 +0000

    first commit for robot functions

---
+++ L204_IDP.ino
@@ -0,0 +1,66 @@
+#include <Servo.h>
+#include "robotDefines.h"
+
+String command;
+String device;
+
+// BT Module
+#include <SoftwareSerial.h>
+//TODO: Enter the value of the port for serial communication
+SoftwareSerial BT1(10, 11); // El pin 10 es Rx y el pin 11 es Tx
+//10 is rxPin which is used to receive serial data
+//---------------------------------------------
+void setup() 
+{
+  
+  Serial.begin(9600);
+  
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  
+  // line follow sensors
+  pinMode(lineFollowSensor0, INPUT);
+  pinMode(lineFollowSensor1, INPUT);
+  pinMode(lineFollowSensor2, INPUT);
+  pinMode(lineFollowSensor3, INPUT);
+  
+  
+  // servos
+  //TODO: Choose the port for motors (right and left)
+  leftServo.attach(5);
+  rightServo.attach(3);
+  
+
+  Serial.println("Line Follower is alive ==> Presss the button to start");
+
+  while (digitalRead(buttonPin))
+  {  
+  }
+  
+}
+
+void loop() 
+{
+    readLFSsensors();    
+    switch (mode)
+  {
+    case STOPPED: 
+      motorStop();
+      ledBlink();
+      previousError = error;
+      break;
+
+    case NO_LINE:  
+      motorStop();
+      motorTurn(LEFT, 180);
+      previousError = 0;
+      break;
+
+    case FOLLOWING_LINE:     
+      calculatePID();
+      motorPIDcontrol();    
+      break;     
+  }
+}
+
---
+++ sensorFunctions.ino
@@ -0,0 +1,38 @@
+
+
+//-------------------------------------------------------------
+/* read line sensors values 
+
+Sensor Array 	Error Value
+0 0 0 1 error = 3
+0 0 1 1 error = 2
+0 0 1 0 error = 1
+0 1 1 0 error = 0
+0 1 0 0 error = 1
+1 1 0 0 error = 2
+1 0 0 0 error = 3            
+
+1 1 1 1        0 Robot found continuous line : STOPPED
+0 0 0 0        0 Robot found no line: turn 180o //TODO: Determine something to do
+
+*/
+void readLFSsensors()
+{
+  LFSensor[0] = digitalRead(lineFollowSensor0);
+  LFSensor[1] = digitalRead(lineFollowSensor1);
+  LFSensor[2] = digitalRead(lineFollowSensor2);
+  LFSensor[3] = digitalRead(lineFollowSensor3);
+ 
+  
+  if((     LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 1 ))  {mode = FOLLOWING_LINE; error = 3;}
+  else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = FOLLOWING_LINE; error = 2;}
+  else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 0 ))  {mode = FOLLOWING_LINE; error = 1;}
+  else if((LFSensor[0]== 0 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 0 ))  {mode = FOLLOWING_LINE; error = 0;}
+  else if((LFSensor[0]== 0 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = FOLLOWING_LINE; error = -1;}
+  else if((LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = FOLLOWING_LINE; error = -2;}
+  else if((LFSensor[0]== 1 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = FOLLOWING_LINE; error = -3;}
+
+
+  else if((LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = STOPPED; error = 0;}
+  else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = NO_LINE; error = 0;}
+}

d98fb77bcb0dc7595ebc6e38db40847c2e743a00
Naseweisssss - Ryan Lin <89737029+naseweisssss@users.noreply.github.com>
2023-03-18 11:11:30 +0000

    Update sensorFunctions.ino

--- Software/L204_IDP/sensorFunctions.ino
+++ Software/L204_IDP/sensorFunctions.ino
@@ -3,14 +3,6 @@
 //-------------------------------------------------------------
 /* read line sensors values 
 
-Sensor Array 	Error Value
-0 0 0 1 error = 3
-0 0 1 1 error = 2
-0 0 1 0 error = 1
-0 1 1 0 error = 0
-0 1 0 0 error = 1
-1 1 0 0 error = 2
-1 0 0 0 error = 3
 
 New line sensor options:
 1 1 0 0 Junction
@@ -40,7 +32,6 @@ void readLFSsensors() {
   LFSensor[3] = digitalRead(lineFollowSensor3);
 
 
-// Where the function below used to be ************************************
 
 
 // Code for printing out the line sensor readings and the current mode
@@ -57,39 +48,13 @@ void readLFSsensors() {
 
 
 void lineFollowingMode(){
-  // Contains the logic for normal line following
-
-
-    // Old code in case I stop something from working :)
-  // if((     LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = RIGHT_LINE;} // Junction
-  // else if((LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 0 ))  {mode = ON_LINE;} // Junction
-  // else if((LFSensor[0]== 0 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = RIGHT_LINE;}
-  // else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = ON_LINE;}
-  //  else if((LFSensor[0]== 0 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 0 ))  {mode = ON_LINE;}
-  // else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 0 ))  {mode = LEFT_LINE;}
-  // else if((LFSensor[0]== 0 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = ON_LINE;} // Junction
-  // else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = LEFT_LINE;} // Junction
-  // else if((LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = STOPPED;} // Leaving box
-
-
 // Code for normal line following (not the corners or the junctions)
   if(    (LFSensor[1]== 1 )&&(LFSensor[2]== 0 ))  {mode = RIGHT_LINE; tight = 0; iniMotorPower = power * steeringSmoothness;}  // Allows for smoother steering as there is less slowing down from the inside wheel
   else if((LFSensor[1]== 0 )&&(LFSensor[2]== 0 ))  {mode = ON_LINE; tight = 0; iniMotorPower = power * steeringSmoothness;}
   else if((LFSensor[1]== 0 )&&(LFSensor[2]== 1 ))  {mode = LEFT_LINE; tight = 0; iniMotorPower = power * steeringSmoothness;}
 
 
-// if((LFSensor[1]== 1 )&&(LFSensor[2]== 0 ))  {mode = RIGHT_LINE; tight = 0; iniMotorPower = power * steeringSmoothness;}  // Allows for smoother steering as there is less slowing down from the inside wheel
-//  else if((LFSensor[1]== 0 )&&(LFSensor[2]== 0 ))  {mode = ON_LINE; tight = 0; iniMotorPower = power * steeringSmoothness;}
-//  else if((LFSensor[1]== 0 )&&(LFSensor[2]== 1 ))  {mode = LEFT_LINE; tight = 0; iniMotorPower = power * steeringSmoothness;}
-
-
-// Code allowing for tighter turning at the corners --> the only time just a single outside sensor should be on is when the robot doesn't turn enough for a corner
-  //else if((LFSensor[0]== 1 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = RIGHT_LINE; tight = 0; iniMotorPower = power * steeringSmoothness; }
-  //else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 1 ))  {mode = LEFT_LINE; tight = 0; iniMotorPower = power * steeringSmoothness; }
-
-// Code which detects the presence of a junction  
-  //else if((LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 0 ))  {mode = JUNCTION;}   // Don't think this state is possible
-  
+ 
   if (outside_junction == 0){
   if((     LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = JUNCTION;}
   else if((     LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = JUNCTION;}// Junction
@@ -101,17 +66,6 @@ void lineFollowingMode(){
     
   }
   
- // Junction
-  //else if((LFSensor[0]== 0 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = JUNCTION;} // Leaving box
-
-// Code for error states --> not currently using as for leaving the squares
-//  else if((LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = ERROR;}
-
-
-
-  //else if((LFSensor[0]== 1 )&&(LFSensor[1]== 1 )&&(LFSensor[2]== 1 )&&(LFSensor[3]== 1 ))  {mode = STOPPED; error = 0;}
-  //else if((LFSensor[0]== 0 )&&(LFSensor[1]== 0 )&&(LFSensor[2]== 0 )&&(LFSensor[3]== 0 ))  {mode = NO_LINE; error = 0;}
-
 }
 
 
